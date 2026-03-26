1f546e84f8cb5a0a11ade9268910ce51489c9399
Ajaybhanushali11 <ajaybhanushali110@gmail.com>
2026-03-26 21:25:09 +0530

    final aadmin

--- admin.py
+++ admin.py
@@ -1,20 +1,33 @@
 from django.contrib import admin
-from .models import Question, Choice, Submission
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission
 
+# Inline for Choice inside Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 2
 
+# Inline for Question inside Lesson
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 1
 
+# Inline for Lesson inside Course
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 1
+
+# Lesson Admin
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+# Question Admin
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
-class LessonAdmin(admin.ModelAdmin):
-    pass
-
+# Register models
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Enrollment)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
