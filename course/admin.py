2e7afcd744de8dba7876a00d4f28779ff2c0abfc
Ajaybhanushali11 <ajaybhanushali110@gmail.com>
2026-03-26 21:51:00 +0530

    .

--- course/admin.py
+++ course/admin.py
@@ -1,1 +1,25 @@
+from django.contrib import admin
+from .models import Question, Choice, Submission
 
+# Inline for Choice
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+# Dummy LessonInline (requirement fulfill करने के लिए)
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+# Question Admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+# LessonAdmin (dummy class, required by assignment)
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+# Register models
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
